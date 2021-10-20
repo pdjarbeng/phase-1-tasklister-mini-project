340341b65f13ffb424e4b772fe035a4955651591
priscilla <pdjarbeng@outlook.com>
2021-10-19 23:50:30 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById('create-task-form').addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log (event.target)
+  })
+
 });
+
+
+
+
+
+
+
+
+
+
+//question why domcontentloaded
